1f04844ede08744705901cf9790dfc8c97d3e0e8
vickyfrosties <sounia.mhalla@hotmail.com>
2024-09-14 22:49:42 +0200

    Quizz - Questions & answers randomized

--- js/quizz.js
+++ js/quizz.js
@@ -1,41 +1,79 @@
 const PLAY_BTN = document.getElementById("start-game");
 const H3 = document.getElementById("lancement");
 
-const QUESTIONS = ["Quelle est la planète la plus proche de la Terre ?", "Quelle est la planète la plus éloignée ?", "Pourquoi appelle-t-on Mars la planète rouge ?", "Qui a été le premier astronaute a avoir mis les pieds sur la Lune ?"];
-
-console.log(QUESTIONS);
-
+const QUESTIONS = [
+    {
+      question: "Quelle est la planète la plus proche du Soleil ?",
+      reponses: ["Mercure", "Vénus", "Terre", "Mars"],
+      bonneReponse: "Mercure"
+    },
+    {
+      question: "Quelle est la plus grande planète du système solaire ?",
+      reponses: ["Jupiter", "Saturne", "Neptune", "Uranus"],
+      bonneReponse: "Jupiter"
+    },
+    {
+      question: "Quelle planète est surnommée la planète rouge ?",
+      reponses: ["Mars", "Vénus", "Mercure", "Neptune"],
+      bonneReponse: "Mars"
+    },
+    {
+      question: "Combien y a-t-il de planètes dans le système solaire ?",
+      reponses: ["7", "8", "9", "10"],
+      bonneReponse: "8"
+    },
+    {
+      question: "Quelle planète possède des anneaux visibles ?",
+      reponses: ["Saturne", "Jupiter", "Mars", "Terre"],
+      bonneReponse: "Saturne"
+    },
+    {
+      question: "Quelle est la planète la plus éloignée du Soleil ?",
+      reponses: ["Neptune", "Uranus", "Pluton", "Saturne"],
+      bonneReponse: "Neptune"
+    },
+    {
+      question: "La Terre est-elle une planète rocheuse ou gazeuse ?",
+      reponses: ["Rocheuse", "Gazeuse"],
+      bonneReponse: "Rocheuse"
+    },
+    {
+      question: "Quelle planète est la plus chaude du système solaire ?",
+      reponses: ["Vénus", "Mercure", "Terre", "Mars"],
+      bonneReponse: "Vénus"
+    },
+    {
+      question: "Quel est le satellite naturel de la Terre ?",
+      reponses: ["Lune", "Mars", "Mercure", "Saturne"],
+      bonneReponse: "Lune"
+    },
+    {
+      question: "Comment s'appelle notre étoile ?",
+      reponses: ["Soleil", "Lune", "Jupiter", "Mars"],
+      bonneReponse: "Soleil"
+    }
+  ];
+  
 PLAY_BTN.addEventListener("click", () => {
-    console.log("prout");
-
     setTimeout(() => {
         H3.style.display = "none";
         PLAY_BTN.style.display = "none";
-        
-        // création des div, p, etc pour les questions + choix
+
         const CONTAINER = document.getElementById("questionnaire-bloc");
-        const QUESTIONS_BLOC = document.createElement("div");
-        const FIRST_QUESTION = document.createElement("p");
-        const F_CHOICE = document.createElement("button");
-        const S_CHOICE = document.createElement("button");
-        const T_CHOICE = document.createElement("button");
-
-    
-        CONTAINER.appendChild(QUESTIONS_BLOC);
-        QUESTIONS_BLOC.append(FIRST_QUESTION, F_CHOICE, S_CHOICE, T_CHOICE);
-    
-        QUESTIONS_BLOC.classList.add("bloc-questions");
-        FIRST_QUESTION.classList.add("first-question");
-        F_CHOICE.classList.add("first-choice");
-        S_CHOICE.classList.add("second-choice");
-        T_CHOICE.classList.add("third-choice");
-
         
-        const RANDOM_QUESTION = QUESTIONS[Math.floor(Math.random()) * 4];
-        FIRST_QUESTION.innerHTML = RANDOM_QUESTION;
-        F_CHOICE.innerHTML = "Mercure";
-        S_CHOICE.innerHTML = "Venus";
-        T_CHOICE.innerHTML = "Mars";
-       
+        const QUESTION = document.createElement("p");
+        CONTAINER.append(QUESTION);
+        const RANDOM_QUESTION = Math.floor(Math.random() * QUESTIONS.length);
+        QUESTION.textContent = QUESTIONS[RANDOM_QUESTION].question;
+        const ANSWERS = QUESTIONS[RANDOM_QUESTION].reponses;
+
+        ANSWERS.forEach((response, index) => {
+            const BTN_ANSWERS = document.createElement("button");
+            BTN_ANSWERS.textContent = `${index + 1}. ${response}`;
+    
+            QUESTION.classList.add("question");
+            BTN_ANSWERS.classList.add("choices");
+            CONTAINER.appendChild(BTN_ANSWERS);
+        });
     }, 1000)
 })
